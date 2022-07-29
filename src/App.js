e44ad1b86d7cff13d561f2f8c6de2d6b22e5d184
HartMason <hartmason85@gmail.com>
2022-07-29 17:48:43 -0500

    added logic for search by author, comments, stories

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import './App.css';
-import {Component} from 'react';
-import Header from './components/Header';
+import "./App.css";
+import { Component } from "react";
+import Header from "./components/Header";
+import axios from "axios";
 
 //hackathon
-//made changes
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor() {
+    super();
 
     this.state = {
+      listOfStories: [],
+      category: "stories",
+      searchParam: "",
+    };
+  }
 
+  componentDidMount = () => {
+    axios
+      .get(
+        "https://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=50"
+      )
+      .then((res) => {
+        console.log(res);
+        this.setState({listOfStories: res.data.hits});
+      });
+  };
+
+  componentDidUpdate = () => {
+    // axios.get(`http://hn.algolia.com/api/v1/search?query=${this.state.category}&hitsPerPage=50`)
+    console.log(this.state);
+  };
+
+  setCategory = (input) => {
+    this.setState({category: input});
+  };
+
+  setSearchParams = (input) => {
+    this.setState({searchParam: input});
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    if (this.state.category === "author") {
+      return axios.get(`http://hn.algolia.com/api/v1/search?tags=story,author_${this.state.searchParam}&hitsPerPage=50`).then((res) => {
+        this.setState({listOfStories: res.data.hits})
+      }) 
     }
+    axios.get(`http://hn.algolia.com/api/v1/search?query=${this.state.searchParam}&hitsPerPage=50`).then((res) => {
+      this.setState({listOfStories: res.data.hits})
+    }) 
   }
 
 
-  render(){
+  render() {
     return (
       <div>
-        <Header />
+        <Header
+          handleSubmit={this.handleSubmit}
+          setCategory={this.setCategory}
+          setSearchParams={this.setSearchParams}
+          category={this.category}
+          searchParam={this.searchParam}
+        />
       </div>
     );
-}}
+  }
+}
 
 export default App;
