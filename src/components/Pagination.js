ef087c609a75f148105c4102383a2c15dd454066
CalebAurand <caurand13@gmail.com>
2022-08-02 21:33:32 -0500

    added Rosalyn's pagination buttons

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,12 +1,22 @@
 import React from 'react';
 
-const Pagination = () => (
-  <div>
-    <button>Button
-    </button>
-    <button>Button</button>
+const Pagination = (props) => (
+
+  <div className="paginationStyle">
+  
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage( )}> ... </span>
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage(1)}>1</span>
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage(2)}>2</span>
+  <span className="pagSpan" conClick={() => this.makeHttpRequestWithPage(3)}>3</span>
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage(4)}>4</span>
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage(5)}>5</span>
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage(6)}>6</span>
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage(7)}>7</span>
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage(8)}>8</span>
+  <span className="pagSpan" onClick={() => this.makeHttpRequestWithPage( )}> ... </span>
+  
   </div>
-)
+  )
 
 export default Pagination;
 
